33af431a64cd0852b2225b430ad15db79659cda0
ubuntu <11>
2025-07-31 17:42:38 +0800

    1111

--- train_net.py
+++ train_net.py
@@ -37,7 +37,7 @@ from diffusioninst.util.model_ema import add_model_ema_configs, may_build_model_
 class Trainer(DefaultTrainer): #11111111111 todo
     """ Extension of the Trainer class adapted to DiffusionInst. """
 
-    def __init__(self, cfg):
+    def __init__(self, 11cfg):
         """
         Args:
             cfg (CfgNode):
